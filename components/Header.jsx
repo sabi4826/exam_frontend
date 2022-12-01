c3448870c77c0a6c80560ac3544f4c7c76d86a18
Sabine <sabi4826@stud.kea.dk>
2022-12-01 16:38:33 +0100

    work on css in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,9 +3,9 @@ import "../src/style/header.css"
 
 export default function Header() {
   return <div className="foo_header">
-            <h3>COPENHAGEN JULY 7TH - JULY 16TH</h3>
+            <h3>COPENHAGEN <br></br>JULY 7TH - JULY 16TH</h3>
             <h1>FOO FEST 2023</h1>
-            <button>Buy tickets!</button>  
+            <button className="buy-ticket-header">Buy tickets!</button>  
   
   </div>;
 }
