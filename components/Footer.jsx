c9ddb255d81d1f00d8fa26c8a5e4745a8351ec21
jasmin <yase0236@stud.kea.dk>
2022-12-01 18:04:32 +0100

    footerstyling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,21 +4,23 @@ import "../src/style/footer.css";
 export default function Footer() {
   return (
     <div className="footer_content">
-      <h2>FooFest</h2>
-      <p>For camping inquiries, ticket purchases, and accessibility arrangements, please reach out to us at:</p>
+      <div className="footer_headlines">
+        <h2>FooFest</h2>
+        <p>For camping inquiries, ticket purchases, and accessibility arrangements, please reach out to us at:</p>
+      </div>
 
-      <div class="footer_information">
+      <div className="footer_information">
         <div className="contact">
           <h3>Contact us</h3>
           <p>
-            POPPELGÅRDSVEJ 6, <br /> KØBENHAVN K 1410
+            Poppelgårdsvej 6, <br /> København K 1410
           </p>
           <p>+45 45 62 83 90</p>
         </div>
 
         <div className="email">
           <h3>Email us</h3>
-          <p>HELLO@FOOFEST.COM</p>
+          <p>Hello@Foofest.com</p>
         </div>
 
         <div className="socials">
