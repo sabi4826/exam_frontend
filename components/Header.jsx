ea7c056b10751311eef3998be39db90241f2cde5
Sabine <sabi4826@stud.kea.dk>
2022-12-02 20:06:11 +0100

    classNames added to h in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,8 +4,8 @@ import "../src/style/index.css";
 
 export default function Header() {
   return <div className="foo_header">
-            <h3>COPENHAGEN <br></br>JULY 7TH - JULY 16TH</h3>
-            <h1>FOO FEST 2023</h1>
+            <h3 className="header_h3">COPENHAGEN <br></br>JULY 7TH - JULY 16TH</h3>
+            <h1 className="header_h1">FOO FEST 2023</h1>
             <button className="buy-ticket-header">BUY TICKETS!</button>  
   
   </div>;
