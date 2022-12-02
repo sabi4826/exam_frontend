795ed556db673fc5129f6a818c9aa0e786269c08
jasmin <yase0236@stud.kea.dk>
2022-12-02 10:41:30 +0100

    styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "../src/style/footer.css";
+import "../src/style/index.css";
 
 export default function Footer() {
   return (
     <div className="footer_content">
       <div className="footer_headlines">
         <h2>FooFest</h2>
-        <p>For camping inquiries, ticket purchases, and accessibility arrangements, please reach out to us at:</p>
+        <p className="footer_text">For camping inquiries, ticket purchases, and accessibility arrangements, please reach out to us at:</p>
       </div>
 
       <div className="footer_information">
         <div className="contact">
           <h3>Contact us</h3>
-          <p>
+          <p className="footer_text">
             Poppelgårdsvej 6, <br /> København K 1410
           </p>
           <p>+45 45 62 83 90</p>
@@ -20,14 +21,14 @@ export default function Footer() {
 
         <div className="email">
           <h3>Email us</h3>
-          <p>Hello@Foofest.com</p>
+          <p className="footer_text">Hello@Foofest.com</p>
         </div>
 
         <div className="socials">
           <h3>Socials</h3>
-          <p>Facebook</p>
-          <p>YouTube</p>
-          <p>Instagram</p>
+          <p className="footer_text">Facebook</p>
+          <p className="footer_text">YouTube</p>
+          <p className="footer_text">Instagram</p>
         </div>
       </div>
     </div>
