8d533689bc1958b92623989ccbf43cb4e8830e5b
jasmin <yase0236@stud.kea.dk>
2022-12-03 17:31:43 +0100

    using props for the different classes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../src/style/footer.module.css";
+import footerStyles from "../src/style/footer.module.css";
 import "../src/style/index.module.css";
 
 import Instagram from "../src/assets/instagram.svg";
@@ -8,39 +8,39 @@ import Facebook from "../src/assets/facebook.svg";
 
 export default function Footer() {
   return (
-    <div className="footer_content">
-      <div className="footer_headlines">
+    <div className={footerStyles.footer_content}>
+      <div className={footerStyles.footer_headlines}>
         <h2>FooFest</h2>
-        <p className="footer_text">For camping inquiries, ticket purchases, and accessibility arrangements, please reach out to us at:</p>
+        <p className={footerStyles.footer_text}>For camping inquiries, ticket purchases, and accessibility arrangements, please reach out to us at:</p>
       </div>
 
-      <div className="footer_information">
-        <div className="contact">
-          <h3 className="headline_text">Contact us</h3>
-          <p className="footer_text">
+      <div className={footerStyles.footer_information}>
+        <div className={footerStyles.contact}>
+          <h3 className={footerStyles.headline_text}>Contact us</h3>
+          <p className={footerStyles.footer_text}>
             Poppelgårdsvej 6, <br /> København K 1410
           </p>
-          <p className="footer_text">+45 45 62 83 90</p>
+          <p className={footerStyles.footer_text}>+45 45 62 83 90</p>
         </div>
 
-        <div className="email">
-          <h3 className="headline_text">Email us</h3>
-          <p className="footer_text">Hello@Foofest.com</p>
+        <div className={footerStyles.email}>
+          <h3 className={footerStyles.headline_text}>Email us</h3>
+          <p className={footerStyles.footer_text}>Hello@Foofest.com</p>
         </div>
 
-        <div className="socials">
-          <h3 className="headline_text">Socials</h3>
+        <div className={footerStyles.socials}>
+          <h3 className={footerStyles.headline_text}>Socials</h3>
 
-          <div className="footer_text" id="socials_logo">
-            <img src={Facebook} className="logos" alt="facebook" />
+          <div className={footerStyles.footer_text} id={footerStyles.socials_logo}>
+            <img src={Facebook} className={footerStyles.logos} alt="facebook" />
           </div>
 
-          <div className="footer_text" id="socials_logo">
-            <img src={Youtube} className="logos" alt="youtube" />
+          <div className={footerStyles.footer_text} id={footerStyles.socials_logo}>
+            <img src={Youtube} className={footerStyles.logos} alt="youtube" />
           </div>
 
-          <div className="footer_text" id="socials_logo">
-            <img src={Instagram} className="logos" alt="instagram" />
+          <div className={footerStyles.footer_text} id={footerStyles.socials_logo}>
+            <img src={Instagram} className={footerStyles.logos} alt="instagram" />
           </div>
         </div>
       </div>
