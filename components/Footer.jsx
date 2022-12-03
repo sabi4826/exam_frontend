df41e9e28c7e383e3b73994fc9bcce95fdf12bfb
jasmin <yase0236@stud.kea.dk>
2022-12-03 17:12:52 +0100

    a lot of changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import "../src/style/footer.css";
-import "../src/style/index.css";
+import "../src/style/footer.module.css";
+import "../src/style/index.module.css";
+
+import Instagram from "../src/assets/instagram.svg";
+import Youtube from "../src/assets/youtube.svg";
+import Facebook from "../src/assets/facebook.svg";
 
 export default function Footer() {
   return (
@@ -12,23 +16,32 @@ export default function Footer() {
 
       <div className="footer_information">
         <div className="contact">
-          <h3>Contact us</h3>
+          <h3 className="headline_text">Contact us</h3>
           <p className="footer_text">
             Poppelgårdsvej 6, <br /> København K 1410
           </p>
-          <p>+45 45 62 83 90</p>
+          <p className="footer_text">+45 45 62 83 90</p>
         </div>
 
         <div className="email">
-          <h3>Email us</h3>
+          <h3 className="headline_text">Email us</h3>
           <p className="footer_text">Hello@Foofest.com</p>
         </div>
 
         <div className="socials">
-          <h3>Socials</h3>
-          <p className="footer_text">Facebook</p>
-          <p className="footer_text">YouTube</p>
-          <p className="footer_text">Instagram</p>
+          <h3 className="headline_text">Socials</h3>
+
+          <div className="footer_text" id="socials_logo">
+            <img src={Facebook} className="logos" alt="facebook" />
+          </div>
+
+          <div className="footer_text" id="socials_logo">
+            <img src={Youtube} className="logos" alt="youtube" />
+          </div>
+
+          <div className="footer_text" id="socials_logo">
+            <img src={Instagram} className="logos" alt="instagram" />
+          </div>
         </div>
       </div>
     </div>
