9ea01bd6e4d9c3e7ed9b84b5bc795d85c6562f46
Sabine <sabi4826@stud.kea.dk>
2022-12-04 10:25:30 +0100

    updated with headerstyle import and classNames.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import "../src/style/header.css";
+import headerStyle from "../src/style/header.css";
 import "../src/style/index.css";
 
 export default function Header() {
-  return <div className="foo_header">
-            <h3 className="header_h3">COPENHAGEN <br></br>JULY 7TH - JULY 16TH</h3>
-            <h1 className="header_h1">FOO FEST 2023</h1>
-            <button className="buy-ticket-header">BUY TICKETS!</button>  
-  
+  return <div className={headerStyle.foo_header}>
+            <h3 className={headerStyle.header_h3}>COPENHAGEN <br></br>JULY 7TH - JULY 16TH</h3>
+            <h1 className={headerStyle.header_h1}>FOO FEST 2023</h1>
+            <button className={headerStyle.buy-ticket-header}>BUY TICKETS!</button>  
   </div>;
 }
