48c8d206c668d47262e4c067d47720ac872dadc5
Nancy <nanc0603@stud.kea.dk>
2022-12-04 12:47:03 +0100

    fixing the header ticket button

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,9 +3,13 @@ import headerStyle from "../src/style/header.module.css";
 import "../src/style/index.module.css";
 
 export default function Header() {
-  return <div className={headerStyle.foo_header}>
-            <h3 className={headerStyle.header_h3}>COPENHAGEN <br></br>JULY 7TH - JULY 16TH</h3>
-            <h1 className={headerStyle.header_h1}>FOO FEST 2023</h1>
-            <button className={headerStyle.buy-ticket-header}>BUY TICKETS!</button>  
-  </div>;
+  return (
+    <div className={headerStyle.foo_header}>
+      <h3 className={headerStyle.header_h3}>
+        COPENHAGEN <br></br>JULY 7TH - JULY 16TH
+      </h3>
+      <h1 className={headerStyle.header_h1}>FOO FEST 2023</h1>
+      <button className={headerStyle.buyTicketHeader}>BUY TICKETS!</button>
+    </div>
+  );
 }
