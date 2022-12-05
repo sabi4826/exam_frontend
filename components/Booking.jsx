5b8c08fb9ce2e38c0f34867e112410ce51acf13b
Nancy <nanc0603@stud.kea.dk>
2022-12-05 08:58:31 +0100

    working in booking section

--- components/Booking.jsx
+++ components/Booking.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "../src/style/booking.css";
-// import "../src/style/index.css";
+import bookingStyles from "../src/style/booking.module.css";
+import "../src/style/index.module.css";
 
 export default function Booking() {
   //   function removeOne() {
@@ -12,13 +12,13 @@ export default function Booking() {
   //     console.log(addSymbol);
   //   }
   return (
-    <div className="booking-section">
-      <h3>TICKETS</h3>
-      <section className="ticket-section">
+    <div className={bookingStyles.booking_section}>
+      <h3 className={bookingStyles.h3_text}>TICKETS</h3>
+      <section className={bookingStyles.ticket_section}>
         <aside>
           <h4>REGULAR TICKETS</h4>
           <p>799,-</p>
-          <form className="quantity-form">
+          <form className={bookingStyles.quantity_form}>
             <input type="number" min="1" max="10" name="quantative" title="Qty" placeholder="Select" inputMode="numeric" id="Qty" />
             {/* <span className="remove-symbol" onClick={removeOne}>
               -
@@ -27,13 +27,13 @@ export default function Booking() {
               {" "}
               +
             </span> */}
-            <button className="add-cart">Add to Cart</button>
+            <button className={bookingStyles.add_cart}>Add to Cart</button>
           </form>
         </aside>
         <aside>
           <h4>VIP TICKETS</h4>
           <p>1299,-</p>
-          <form className="quantity-form">
+          <form className={bookingStyles.quantity_form}>
             <input type="number" min="1" max="10" name="quantative" title="Qty" placeholder="Select" inputMode="numeric" id="Qty" />
             {/* <span className="remove-symbol" onClick={removeOne}>
               -
@@ -42,13 +42,13 @@ export default function Booking() {
               {" "}
               +
             </span> */}
-            <button className="add-cart">Add to Cart</button>
+            <button className={bookingStyles.add_cart}>Add to Cart</button>
           </form>
         </aside>
         <aside>
           <h4>GREEN CAMPING</h4>
           <p>+249,-</p>
-          <form className="quantity-form">
+          <form className={bookingStyles.quantity_form}>
             <input type="number" min="1" max="10" name="quantative" title="Qty" placeholder="Select" inputMode="numeric" id="Qty" />
             {/* <span className="remove-symbol" onClick={removeOne}>
               -
@@ -57,14 +57,14 @@ export default function Booking() {
               {" "}
               +
             </span> */}
-            <button className="add-cart">Add to Cart</button>
+            <button className={bookingStyles.add_cart}>Add to Cart</button>
           </form>
         </aside>
         <aside>
           <h4>2 PERSON TENT</h4>
           <p>299,-</p>
-          <h5 className="h5-text">Fixed booking fee 99,-</h5>
-          <form className="quantity-form">
+          <h5 className={bookingStyles.h5_text}>Fixed booking fee 99,-</h5>
+          <form className={bookingStyles.quantity_form}>
             <input type="number" min="1" max="10" name="quantative" title="Qty" placeholder="Select" inputMode="numeric" id="Qty" />
             {/* <span className="remove-symbol" onClick={removeOne}>
               -
@@ -73,14 +73,14 @@ export default function Booking() {
               {" "}
               +
             </span> */}
-            <button className="add-cart">Add to Cart</button>
+            <button className={bookingStyles.add_cart}>Add to Cart</button>
           </form>
         </aside>
         <aside>
           <h4>3 PERSON TENT</h4>
           <p>399,-</p>
-          <h5 className="h5-text">Fixed booking fee 99,-</h5>
-          <form className="quantity-form">
+          <h5 className={bookingStyles.h5_text}>Fixed booking fee 99,-</h5>
+          <form className={bookingStyles.quantity_form}>
             <input type="number" min="1" max="10" name="quantative" title="Qty" placeholder="Select" inputMode="numeric" id="Qty" />
             {/* <span className="remove-symbol" onClick={removeOne}>
               -
@@ -89,28 +89,62 @@ export default function Booking() {
               {" "}
               +
             </span> */}
-            <button className="add-cart">Add to Cart</button>
+            <button className={bookingStyles.add_cart}>Add to Cart</button>
           </form>
         </aside>
         <aside>
           <h4>HAVE CREW SET UP YOUR TENT</h4>
+          <p>FREE!!!</p>
+          <form className={bookingStyles.quantity_form}>
+            <input type="number" min="1" max="10" name="quantative" title="Qty" placeholder="Select" inputMode="numeric" id="Qty" />
+            {/* <span className="remove-symbol" onClick={removeOne}>
+              -
+            </span>
+            <span className="add-symbol" onClick={addOne}>
+              {" "}
+              +
+            </span> */}
+            <button className={bookingStyles.add_cart}>Add to Cart</button>
+          </form>
         </aside>
       </section>
-      <button className="booking-ticket-button">BUY TICKETS</button>
-      <section className="form_fields">
+      <button className={bookingStyles.booking_ticket_button}>BUY TICKETS</button>
+
+      <section className={bookingStyles.form_fields}>
         <form action="">
-          <fieldset className="form_styling">
+          <fieldset className={bookingStyles.form_styling}>
             <legend>
-              <h3>Personal Information</h3>
+              <h3 className={bookingStyles.h3_text}>Personal Information</h3>
             </legend>
-            <label htmlFor="fullname">Name:</label>
-            <input type="text" name="Fullname" placeholder="Full Name"></input>
-            <label htmlFor="phone">Phone:</label>
-            <input type="number" name="Fullname" placeholder="ex.1234556"></input>
-            <label htmlFor="email">E-mail:</label>
-            <input type="text" name="Fullname" placeholder="name@mail.com"></input>
+            <label className={bookingStyles.label_text} htmlFor="fullname">
+              Name:
+            </label>
+            <input className={bookingStyles.input_text} type="text" name="Fullname" placeholder="Full Name"></input>
+            <label className={bookingStyles.label_text} htmlFor="phone">
+              Phone:
+            </label>
+            <input className={bookingStyles.input_text} type="number" name="Fullname" placeholder="ex.1234556"></input>
+            <label className={bookingStyles.label_text} htmlFor="email">
+              E-mail:
+            </label>
+            <input className={bookingStyles.input_text} type="text" name="Fullname" placeholder="name@mail.com"></input>
           </fieldset>
         </form>
+      </section>
+      <section className={bookingStyles.calc_text}>
+        <h4>
+          1 Regular Ticket <br />
+          799,-
+        </h4>
+        <h4>
+          2 Persons Tent (tent incl.)
+          <br />
+          299,-
+        </h4>
+        <h4>
+          Fixed booking Fee <br />
+          99,-
+        </h4>
       </section>
     </div>
   );
