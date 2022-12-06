c8a7a4d6ca771dd8fdf815bec7e8110cede193f8
Sabine <sabi4826@stud.kea.dk>
2022-12-06 10:58:23 +0100

    code from insomnia added to db.js as a comment

--- modules/db.js
+++ modules/db.js
@@ -2,7 +2,7 @@ export async function insertOrder(payload) {
   // key = anon key from settings - API:
   const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2eHJuanpxaGNsa3pvZmN4bXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk4OTM5MDUsImV4cCI6MTk4NTQ2OTkwNX0.aV4aetggMNyynspHkRyNQK9u-HuwGytYHI6OTl9SSsE";
   const projectUrl = "https://cvxrnjzqhclkzofcxmwe.supabase.co";
-  const res = await fetch(projectUrl + "/foofest-exam", {
+  const res = await fetch(projectUrl + "/foofest-booking", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -15,3 +15,21 @@ export async function insertOrder(payload) {
   const data = await res.json();
   return data;
 }
+
+// code from insomnia - remember the table is called "foofest-booking" !!!:
+
+/* const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2eHJuanpxaGNsa3pvZmN4bXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk4OTM5MDUsImV4cCI6MTk4NTQ2OTkwNX0.aV4aetggMNyynspHkRyNQK9u-HuwGytYHI6OTl9SSsE',
+    prefer: 'return=representation',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2eHJuanpxaGNsa3pvZmN4bXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk4OTM5MDUsImV4cCI6MTk4NTQ2OTkwNX0.aV4aetggMNyynspHkRyNQK9u-HuwGytYHI6OTl9SSsE'
+  },
+  body: '{"name":"Sabine Ovesen","email":"sabineov@gmail.com","phone":"22426385"}'
+};
+
+fetch('https://cvxrnjzqhclkzofcxmwe.supabase.co/rest/v1/foofest-booking', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err)); */
