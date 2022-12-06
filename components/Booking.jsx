e8d41b66ef82d3b137919136d8190b68544aa98d
jasmin <yase0236@stud.kea.dk>
2022-12-06 10:46:43 +0100

    credit card form

--- components/Booking.jsx
+++ components/Booking.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useState, useRef } from "react";
+
 import bookingStyles from "../src/style/booking.module.css";
 import "../src/style/index.module.css";
 
 export default function Booking(props) {
   const theForm = useRef(null);
-  const [paymentCompleted, setPaymentCompleted] = useState(false);
+  const [infoCompleted, setInfoCompleted] = useState(false);
 
   async function submit(e) {
     e.preventDefault();
@@ -18,33 +19,36 @@ export default function Booking(props) {
 
       basket: props.cart,
     });
-    setPaymentCompleted(true);
+    setInfoCompleted(true);
   }
   return (
     <div className={bookingStyles.booking_section}>
       <section className={bookingStyles.form_fields}>
-        {paymentCompleted ? (
-          <p>Thank you for your purchase</p>
+        {infoCompleted ? (
+          <p>Go to payment</p>
         ) : (
           <form onSubmit={submit} ref={theForm}>
             <fieldset className={bookingStyles.form_styling}>
               <legend>
                 <h3 className={bookingStyles.h3_text}>Personal Information</h3>
               </legend>
+
               <label className={bookingStyles.label_text} htmlFor="fullname">
                 Name:
               </label>
               <input className={bookingStyles.input_text} type="text" name="Fullname" required placeholder="Full Name"></input>
+
               <label className={bookingStyles.label_text} htmlFor="phone">
                 Phone:
               </label>
               <input className={bookingStyles.input_text} type="number" name="Phone" required placeholder="ex.1234556"></input>
+
               <label className={bookingStyles.label_text} htmlFor="email">
                 E-mail:
               </label>
               <input className={bookingStyles.input_text} type="email" name="Email" required placeholder="name@mail.com"></input>
             </fieldset>
-            <button className={bookingStyles.booking_ticket_button}>Payment</button>
+            <button className={bookingStyles.booking_ticket_button}>Go to payment</button>
           </form>
         )}
       </section>
--- components/CreditCard.jsx
+++ components/CreditCard.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import { useState, useRef } from "react";
+
 import Booking from "./Booking";
 import bookingStyles from "../src/style/booking.module.css";
 
-export default function CreditCard() {
-  return <div>CreditCard</div>;
+export default function CreditCard(props) {
+  const creditCardForm = useRef(null);
+  const [paymentCompleted, setPaymentCompleted] = useState(false);
+
+  async function submit(e) {
+    e.preventDefault();
+
+    const response = await insertOrder({
+      cardNumber: creditCardForm.elements.cardNumber.value,
+
+      cardMonth: creditCardForm.elements.cardMonth.value,
+
+      cardYear: creditCardForm.elements.cardYear.value,
+
+      cvcNumber: creditCardForm.elements.cvcNumber.value,
+    });
+    setPaymentCompleted(true);
+  }
+
+  return (
+    <div className="bookingStyles.creditSection">
+      <section className="bookingStyles.creditFormFields">
+        {paymentCompleted ? (
+          <p>Thank you for your purchase</p>
+        ) : (
+          <form onSubmit={submit} ref={creditCardForm}>
+            <fieldset className="bookingStyles.creditFormStyling">
+              <legend>
+                <h3 className="">Credit card information</h3>
+              </legend>
+
+              <label className="" htmlFor="creditCardNumber">
+                Insert your credit card number
+              </label>
+              <input className="" type="number" name="creditCardNumber" required placeholder="Credit Card Number" pattern="[0-9]{16}"></input>
+
+              <label className="" htmlFor="creditCardMonth">
+                Credit card month
+              </label>
+              <input className="" type="number" name="creditCardMonth" required placeholder="Month" pattern="[1-12]{2}"></input>
+
+              <label className="" htmlFor="creditCardYear">
+                Credit card year
+              </label>
+              <input className="" type="number" name="creditCardYear" required placeholder="Year" pattern="[1-12]{2}"></input>
+
+              <label className="" htmlFor="creditCardCvc">
+                CVC number
+              </label>
+              <input className="" type="number" name="CVC" required placeholder="CVC" pattern="[0-9]{2}"></input>
+            </fieldset>
+            <button className={bookingStyles.booking_ticket_button}>Complete payment</button>
+          </form>
+        )}
+      </section>
+    </div>
+  );
 }
