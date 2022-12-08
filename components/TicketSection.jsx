bc45ca7d4021f2552aaa4f9a231fdc1d6e16ad57
Nancy <nanc0603@stud.kea.dk>
2022-12-08 09:12:52 +0100

    add styling to the camping section

--- components/TicketSection.jsx
+++ components/TicketSection.jsx
@@ -2,25 +2,26 @@ import Tickets from "./Tickets";
 import Camping from "./Camping";
 import bookingStyles from "../src/style/booking.module.css";
 import "../src/style/index.module.css";
-
+import campingStyles from "../src/style/camping.module.css";
 export default function TicketSection(props) {
   return (
     <>
-        <article>
-          <section className={bookingStyles.ticket_section}>
-            {props.products.map((product) => (
-              <Tickets key={product.id} data={product} addToCart={props.addToCart} />
-            ))}
-          </section>
-          
-          <section className={bookingStyles.ticket_section}>
-            <h4>Camping spots</h4>
-            <p>Only choose one area per booking!</p>
-            {props.camping.map((camping) => (
-              <Camping key={camping.area} response={camping} addToCart={props.addToCart} />
-            ))}
-          </section>
-        </article>
+      <article>
+        <section className={bookingStyles.ticket_section}>
+          {props.products.map((product) => (
+            <Tickets key={product.id} data={product} addToCart={props.addToCart} />
+          ))}
+        </section>
+        <aside className={campingStyles.camping_heading}>
+          <h4 className={campingStyles.camping_h4}>Camping spots</h4>
+          <p className={campingStyles.camping_p}>Only choose one area per booking!</p>
+        </aside>
+        <section className={campingStyles.camping_container}>
+          {props.camping.map((camping) => (
+            <Camping key={camping.area} response={camping} addToCart={props.addToCart} />
+          ))}
+        </section>
+      </article>
     </>
   );
 }
