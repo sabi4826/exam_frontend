c401d655f71eacb66dd0b2b912d785c1d48cf914
Sabine <sabi4826@stud.kea.dk>
2022-12-19 14:08:26 +0100

    commit again

--- components/Tickets.jsx
+++ components/Tickets.jsx
@@ -8,12 +8,9 @@ function Tickets(props) {
   return (
     <>
       <article className={bookingStyles.articleSection}>
-        <h4>{props.data.name}</h4>
+      <h4 className={bookingStyles.h4_text}>{props.data.name}</h4>
         <p className={campingStyles.camping_p}>{props.data.info}</p>
-        <p>{props.data.price},-</p>
-
-        <h4 className={bookingStyles.h4_text}>{props.data.name}</h4>
-        <p className={bookingStyles.p_text}>{props.data.price},-</p>
+        <p className={bookingStyles.p_text}>{props.data.price},-</p>   
         <button onClick={add} className={bookingStyles.add_cart}>
           Add to Cart
         </button>
