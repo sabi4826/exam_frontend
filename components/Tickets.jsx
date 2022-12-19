18badf443e70809ad1a31286570081e1f23d5d5d
Nancy <nanc0603@stud.kea.dk>
2022-12-19 09:46:52 +0100

    remove added

--- components/Tickets.jsx
+++ components/Tickets.jsx
@@ -3,15 +3,13 @@ import bookingStyles from "../src/style/booking.module.css";
 function Tickets(props) {
   function add() {
     props.addToCart(props.data);
-    // console.log((document.querySelectorAll("bookingStyles.add_cart").innerHTML = "Add More!"));
-    document.querySelector("#added").innerHTML = "Add More!";
   }
   return (
     <>
       <article className={bookingStyles.articleSection}>
         <h4>{props.data.name}</h4>
         <p>{props.data.price},-</p>
-        <button onClick={add} id="added" className={bookingStyles.add_cart}>
+        <button onClick={add} className={bookingStyles.add_cart}>
           Add to Cart
         </button>
       </article>
