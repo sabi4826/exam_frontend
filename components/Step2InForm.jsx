f8ca59c2cb36111391312e23e11a467887f828f3
jasmin <yase0236@stud.kea.dk>
2022-12-20 18:41:13 +0100

    comments

--- components/Step2InForm.jsx
+++ components/Step2InForm.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import GuestInfo from './GuestInfo';
-import CreditCard from './CreditCard';
+import React from "react";
+import GuestInfo from "./GuestInfo";
+import CreditCard from "./CreditCard";
 
 export default function Step2InForm(props) {
+  // decide what to show in form - GuestInfo or CreditCard:
+  if (props.totalTickets >= 2) {
+    return <GuestInfo totalTickets={props.totalTickets} reservationID={props.reservationID} />;
+  } else {
+    return <CreditCard reservationID={props.reservationID} />;
+  }
 
-    // decide what to show in form - GuestInfo or CreditCard:
-    if(props.totalTickets >= 2) {
-        return <GuestInfo totalTickets={props.totalTickets} reservationID={props.reservationID} />
-    } else {
-        return <CreditCard reservationID={props.reservationID}/>
-    }
-
-  return (
-   <></>
-  )
+  return <></>;
 }
